3616729019d2796cc1e19a589b5c17d07f1be22f
andre-11c <andrechipanarios.11@gmail.com>
2025-10-02 13:57:42 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/com/vocatio/VocatioApiApplication.java
@@ -0,0 +1,13 @@
+package com.vocatio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocatioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocatioApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vocatio/VocatioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocatio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocatioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
